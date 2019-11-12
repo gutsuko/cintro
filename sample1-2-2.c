b83056b82e6c9f55d65b68d9e28b45f6b63aa2a1
gutsuko <s1711579@s.tsukuba.ac.jp>
2019-11-12 12:55:03 +0900

    11_12

---
+++ sample1-2-2.c
@@ -0,0 +1,7 @@
+// sample 1-2-2
+#include <stdio.h>
+int main(){
+    char str1[] = "こんにちは";
+    char str2[] = "大河さん";
+    printf("%s%s!\n", str1, str2);
+}
---
+++ sample1-3-2.c
@@ -0,0 +1,12 @@
+// sample 1-3-2
+#include <stdio.h>
+int main(){
+  int x;
+  int y;
+  printf("Type in the first number\n");
+  scanf("%d", &x);
+  printf("Type in the second number\n");
+  scanf("%d", &y);
+  int sum = x + y;
+  printf("The sum is %d\n", sum);
+}
